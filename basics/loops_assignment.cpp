1c39c93d700847d8b90f8aec0304ed2d9f3ccecd
Digvijay <digvijayshahi08@gmail.com>
2026-04-30 02:11:57 +0530

    solved more question

--- basics/loops_assignment.cpp
+++ basics/loops_assignment.cpp
@@ -130,14 +130,117 @@
 // Number of even integers
 // Number of odd integers
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n,x;
+//    cin>>n;
+//    int positive =0 , negative=0, even =0, odd=0;
+//    for (int i = 1 ; i<=n ; i++){
+//     cin>>x;
+
+//    if(x>0){
+//     positive++;
+//    }
+//    else if(x<0){
+//     negative++;
+//    }
+
+//    if(x%2==0){
+//     even++;
+//    }
+//    else{
+//     odd++;
+//    }
+
+//    }
+//     cout << positive << endl;
+//     cout << negative << endl;
+//     cout << even << endl;
+//     cout << odd << endl;
+//     return 0;
+// }
+
+
+//Sum of First N Natural Numbers
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n;
+//    cin>>n;
+//      long long sum = 0;
+//    for (int i = 1 ; i<= n ; i++){
+  
+//     sum = i + sum;    
+// }
+// cout<<sum;
+   
+//     return 0;
+// }
+
+// Factorial
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n;
+//    cin>>n;
+//      long long sum = 1;
+//    for (int i = 1 ; i<= n ; i++){
+//     sum = i * sum;    
+// }
+// cout<<sum;
+   
+//     return 0;
+// }
+// You are given two integers x
+//  and n
+// .
+
+// Compute:
+
+// xn=x×x×⋯×xn times
+
+// Input
+// The first line contains two integers x
+//  and n
+//  (−10≤x≤10
+// , 0≤n≤20
+// ).
+
+// Output
+// Print the value of xn
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int x,n;
+//    cin>>x>>n;
+//    long long sum = 1;
+//    for (int i = 1 ; i<= n ; i++){
+//     sum = sum * x;    
+// }
+// cout<<sum;
+   
+//     return 0;
+// }
+
+
+//Print Number in Reverse
+
+
 #include<iostream>
 using namespace std;
 int main(){
-   int n;
+   long long n;
    cin>>n;
-   for (int i = 1 ; i<= 10 ; i++){
-    cout << n << " * " << i << " = " << n*i<< endl;
-   }
-   
+
+while(n>0){
+    cout<< n %10;
+    n = n/10;
+}   
     return 0;
 }
